8c0e928ba458f0645c504b6857ee365bae22807e
Phong Ho <ho.phongt@gmail.com>
2018-03-31 18:55:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function viewCart() {
             
             }
             
-            if ((i === cart.length - 1)) {
+            if ((i === (cart.length - 1))) {
               for (var x = 0; x === (cart.length - 1); x++)
             var lastLine = `and ${getCart()[x].itemName} at $${getCart()[x].itemPrice}.`;
             }
